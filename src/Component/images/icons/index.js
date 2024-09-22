c3a618fd2a7be9b0edb3931d27b035757f9e2962
Paulo Michael <paulprogrammer947@gmail.com>
2024-09-22 13:42:25 +0300

    new version

--- src/Component/images/icons/index.js
+++ src/Component/images/icons/index.js
@@ -16,6 +16,15 @@ import cppIcon from './icons8-c++-480.png';
 import sanityIcon from './sanity.png';
 import barge from '../barge.png';
 import ecommerceImg from "../domino-164_6wVEHfI-unsplash.jpg";
+import javaIcon from "../icons/java-original-wordmark.svg";
+import photoshopIcon from "../icons/adobe-photoshop.svg";
+import adobeXdIcon from "../icons/adobe-xd.svg";
+import androidIcon from "../icons/android-studio.svg";
+import figmaIcon from "../icons/figma.svg";
+import jspIcon from "../icons/file-type-jsp.svg";
+import swiftIcon from "../icons/swift.svg";
+import flutterIcon from "../icons/flutter.svg";
+import mongoIcon from "../icons/mongodb-original-wordmark.svg";
 export {
-    reactIcon, gitIcon, githubIcon, batchIcon, cssIcon, bootstrapIcon, strapiIcon, jsIcon, reactIcon2, cloudinaryIcon, phpIcon, pythonIcon, linuxIcon, nodeIcon,sanityIcon,cppIcon,  barge, ecommerceImg
+    reactIcon, gitIcon, githubIcon, batchIcon, cssIcon, bootstrapIcon, strapiIcon, jsIcon, reactIcon2, cloudinaryIcon, phpIcon, pythonIcon, linuxIcon, nodeIcon,sanityIcon,cppIcon,  barge, ecommerceImg, javaIcon, photoshopIcon, adobeXdIcon, androidIcon, figmaIcon, jspIcon, swiftIcon, flutterIcon, mongoIcon
 }
